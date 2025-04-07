72679687335b7e01267a4104daed64b6e36e1938
sxperlinx <102521739+sxperlinx@users.noreply.github.com>
2025-04-07 10:45:17 +0000

    Added force static to metadata files

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,8 @@
 import { Robots } from '@lib/types';
 import Meta from '@config/meta';
 
+export const dynamic = 'force-static';
+
 export default function robots(): Robots {
 	return Meta.robots;
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { Sitemap } from '@lib/types';
 import Meta from '@config/meta';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): Sitemap {
 	return Meta.sitemap;
 }
