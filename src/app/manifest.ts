efcca9d7391e55191a580685bb422f6799ced804
sxperlinx <102521739+sxperlinx@users.noreply.github.com>
2025-04-07 10:41:57 +0000

    Force static in manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,6 +1,8 @@
 import { Manifest } from '@lib/types';
 import Meta from '@config/meta';
 
+export const dynamic = 'force-static';
+
 export default function manifest(): Manifest {
 	return Meta.manifest;
 }
